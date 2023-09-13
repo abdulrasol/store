e0cffd02c1af342e3ece9d2e3063db3328edde9a
AbdulRasol S. Al-Hilo <abdulrsol97@gmail.com>
2023-09-13 07:58:27 +0300

    fixing

--- lib/database/models/user_model.dart
+++ lib/database/models/user_model.dart
@@ -1,13 +1,7 @@
-import 'dart:typed_data';
-
 class UserModel {
-  final String profileImage;
-  late Uint8List imageData;
+  String profileImage;
 
   UserModel({
     required this.profileImage,
-  }) {
-    List<int> data = profileImage.codeUnits;
-    Uint8List bytes = Uint8List.fromList(data);
-  }
+  });
 }
